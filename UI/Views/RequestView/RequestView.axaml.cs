12dd771c2da52edda17681b8cd098130dde0943a
LIM <SlashV83@gmail.com>
2023-05-07 13:08:51 +0300

    TASK-4: Add RequestView

---
+++ UI/Views/RequestView/RequestView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UI.Views.RequestView;
+
+public partial class RequestView : UserControl
+{
+    public RequestView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
